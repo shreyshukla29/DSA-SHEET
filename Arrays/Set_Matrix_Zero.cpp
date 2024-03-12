119b6c079fe0c4e808dfa67d15f43af93044dccd
shrey <shreyshukla101217@gmail.com>
2024-03-12 21:13:35 +0530

    Day 1

---
+++ Arrays/Set_Matrix_Zero.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        unordered_set<int> setRows; 
+        unordered_set<int> setColumns; 
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    setRows.insert(i);
+                    setColumns.insert(j);
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(setRows.count(i) > 0 || setColumns.count(j) > 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+int main(){
+    int rows ,cols;
+    cin>>rows>>cols;
+    vector<vector<int>>matrix(rows ,vector<int> (cols));
+    for(int i = 0 ;i<rows ;i++){
+        for(int j=0;j<cols;j++){
+            cin>>matrix[i][j];
+        }
+    }
+    return 0;
+}
