dbfbbea9001a5b951e16a97bd18303a018b529b2
shrey <shreyshukla101217@gmail.com>
2024-03-27 22:08:41 +0530

    progress

--- LinkedList Part 1/Delete_given_node_in_linkedlist.cpp
+++ LinkedList Part 1/Delete_given_node_in_linkedlist.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
+     struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
 public:
     void deleteNode(ListNode* node) {
         
